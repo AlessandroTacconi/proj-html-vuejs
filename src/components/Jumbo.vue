def46b1f359bd8ec101e27bc02f98a6aa47a7d66
AlessandroTacconi <alessandro.tacconi.97@gmail.com>
2023-12-20 16:50:05 +0100

    feat: fix-jumbo

--- src/components/Jumbo.vue
+++ src/components/Jumbo.vue
@@ -8,11 +8,11 @@ export default {
 <template>
   <!-- JUMBO -->
   <div class="jumbo flex">
-    <div class="jumbo-text flex container">
+    <div class="jumbo-text flex container2">
       <h2>Udemy Affiliate Sales</h2>
       <p>Monitize your audience and attract new customers with Udemy!</p>
     </div>
-    <div class="prova"><img src="/img/slide-1.jpg" alt="foto di gruppo" /></div>
+    <div class="image"><img src="/img/slide-1.jpg" alt="foto di gruppo" /></div>
   </div>
   <!-- /JUMBO -->
 </template>
@@ -25,15 +25,16 @@ export default {
   .jumbo-text {
     flex-direction: column;
     padding-left: 20px;
-
+    max-width: 650px;
     gap: 30px;
+
     h2 {
       font-size: 45px;
     }
   }
 }
 
-.prova {
+.image {
   margin-left: auto;
   clip-path: polygon(0 0, 100% 0, 100% 100%, 20% 100%);
 }
